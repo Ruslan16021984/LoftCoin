ecbd74859158f655274e36ed629512222bce2859
Ruslan16021984 <carbit3333333@gmail.com>
2021-05-27 12:12:53 +0300

    commit2

--- app/src/main/java/com/carbit3333333/loftcoin/ui/splash/SplashActivity.java
+++ app/src/main/java/com/carbit3333333/loftcoin/ui/splash/SplashActivity.java
@@ -2,7 +2,6 @@ package com.carbit3333333.loftcoin.ui.splash;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,8 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
 import com.carbit3333333.loftcoin.R;
-import com.carbit3333333.loftcoin.databinding.ActivitySplashBinding;
-import com.carbit3333333.loftcoin.databinding.ActivityWelcomeBinding;
 import com.carbit3333333.loftcoin.ui.main.MainActivity;
 import com.carbit3333333.loftcoin.ui.welcome.WelcomeActivity;
 
@@ -20,16 +17,11 @@ public class SplashActivity extends AppCompatActivity {
     private SharedPreferences prefs;
     private final Handler handler = new Handler();
     private Runnable goNext;
-    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySplashBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        ClipDrawable clipDrawable = (ClipDrawable) binding.clipArtboard2.getDrawable();
-        clipDrawable.setLevel(5000);
-
+        setContentView(R.layout.activity_splash);
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (prefs.getBoolean(WelcomeActivity.KEY_SHOW_WELCOME, true)) {
             goNext = () ->
